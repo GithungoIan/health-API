03fe5a9a8d3874b12faabb08a98f0e105dd94789
GithungoIan <githungoian53@gmail.com>
2021-09-10 07:04:22 +0300

    created server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXEPTION 🔥 ..shutting down");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./.env" });
+const app = require("./app");
+
+const DB = process.env.DB_LOCAL;
+const port = process.env.PORT;
+
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB connection established"));
+
+const server = app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(" unhandledRejection! 🔥  Shuting down....");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
