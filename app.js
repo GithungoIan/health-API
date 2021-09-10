780418fd7fab5d0b3c2852f00389e4d9fd3f2277
GithungoIan <githungoian53@gmail.com>
2021-09-10 07:58:04 +0300

    added api routes

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 
+const patientRouter = require("./routes/patientRoutes");
+const facilityRouter = require("./routes/facilityRoutes");
+const departmentRouter = require("./routes/departmentRoutes");
+const appointmentRouter = require("./routes/appointmentRoutes");
+const AppError = require("./utils/appError");
+
 const app = express();
 
 //  Set Security HTTP headers
@@ -12,8 +18,20 @@ app.use(helmet());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(cookieParser());
+
+//Routes
+app.use("/api/v1/patients", patientRouter);
+app.use("/api/v1/facilities", facilityRouter);
+app.use("/api/v1/departments", departmentRouter);
+app.use("/api/v1/appointments", appointmentRouter);
+
+// Test Middlewre
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 400));
+});
 
 module.exports = app;
